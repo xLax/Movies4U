959109246027dbc14b40265f3ac7e3bddf5a9cc6
xLax <30602376+xLax@users.noreply.github.com>
2018-09-03 15:09:26 +0300

    Basic Classes

---
+++ Movies4U/Movies4U/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies4U.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+
+        public int NumOfMinutes { get; set; }
+
+        public string Summary { get; set; }
+
+        public int MinimumAge { get; set; }
+
+    }
+}
---
+++ Movies4U/Movies4U/Models/Users.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Movies4U.Models
+{
+    public class Users
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime Birthdate { get; set; }
+    }
+}
