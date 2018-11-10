95aaf42afe6061108216064963db3708c77ec8b7
chen7796 <43431768+chen7796@users.noreply.github.com>
2018-11-10 15:04:23 +0200

    1. Adding home page 2. Adding html5 tags: video, Aside, footer, header, nav, section 3. Adding css3: text-shadow, transition, border-radius 4. Removing not wanted links form login and register pages

--- Movies4U/Movies4U/Controllers/UsersController.cs
+++ Movies4U/Movies4U/Controllers/UsersController.cs
@@ -214,7 +214,7 @@ namespace Movies4U.Controllers
                             HttpContext.Session.SetString("isAdmin", "true");
                         }
                         ViewBag.Error = null;
-                        return RedirectToAction("Index", "Movies");
+                        return RedirectToAction("Home", "Users");
                     }
                 }
             }
@@ -233,7 +233,7 @@ namespace Movies4U.Controllers
                 HttpContext.Session.Clear();
             }
 
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Users");
         }
 
         // GET: Users/Admin
